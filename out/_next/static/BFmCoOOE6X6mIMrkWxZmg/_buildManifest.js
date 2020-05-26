fdadc5f23c81e1e077f8d91f2fc92aebc31b3f21
Aj <jayanwana@gmail.com>
2020-05-26 13:48:29 +0100

    Deploy Next.js to gh-pages

---
+++ out/_next/static/BFmCoOOE6X6mIMrkWxZmg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/admin":[a,b],"/index":[a,b],"/login":[a]}}("static\u002Fchunks\u002F8d65824ad4b01c268aebe4f5f953b1b231e1d5e8.a9075d1af76c094f56b1.js","static\u002Fchunks\u002F98988510442daca2a56e7b6a22e16e4354e44e8a.87759eab11458892e495.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
